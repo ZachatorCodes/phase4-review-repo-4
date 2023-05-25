06967cba07556286d1f64283d9d9fbcc6de9f1fd
alex lieberman <alex.lieberman182@gmail.com>
2023-05-25 08:34:20 -0700

    added validations

--- app/models/encabulator.rb
+++ app/models/encabulator.rb
@@ -1,2 +1,7 @@
 class Encabulator < ApplicationRecord
+    has_many :reviews
+    has_many :users, through: :reviews
+
+    validates :name, presence: true
+    validates :description, presence: true, length: { minimum: 50 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :reviews
     has_many :users, through: :reviews
+
+    validates :username, presence: true, uniqueness: true
+    validates :name, presence: true
 end
