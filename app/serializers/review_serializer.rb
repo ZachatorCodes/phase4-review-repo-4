5e4052d0096c3b559d3d455c240c6a3214a90b6e
alex lieberman <alex.lieberman182@gmail.com>
2023-05-25 08:27:54 -0700

    edited serializers

--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,3 +1,6 @@
 class ReviewSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :encabulator_id, :body
+
+  belongs_to :encabulator
+  belongs_to :user, serializer: ReviewUserSerializer
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :name, :bio
+
+  has_many :reviews
+  has_many :users, through: :reviews
 end
