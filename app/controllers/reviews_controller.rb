cef4b79c36a1e5df1c8febb7a43abb8ba03b237e
alex lieberman <alex.lieberman182@gmail.com>
2023-06-08 14:25:40 -0700

    added security

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,7 @@
 class ReviewsController < ApplicationController
     def create
-        review = Review.create(review_params)
+        user = User.find_by(id: session[:user_id])
+        review = user.reviews.create(review_params)
         if review.valid?
             render json: review, status: :created
         else
@@ -20,14 +21,17 @@ class ReviewsController < ApplicationController
     end
 
     def update
-        review = Review.find_by(id: params[:id])
-        review.update(review_params)
-        render json: review, status: :created
+        review = Review.find_by(id: params[:id])        
+        if review.update(review_params)
+            render json: review, status: :created
+        else
+            render json: { errors: review.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     private
 
     def review_params
-        params.require(:review).permit(:user_id, :encabulator_id, :body)
+        params.require(:review).permit(:encabulator_id, :body)
     end
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,4 @@ class Review < ApplicationRecord
     belongs_to :user
 
     validates :body, presence: true, length: { minimum: 20 }
-    validates :user_id, presence: true
-    validates :encabulator_id, presence: true
 end
